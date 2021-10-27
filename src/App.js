142aa7258506623a017536f049f318fd6caad234
LaloJami <eduardolalojami@gmail.com>
2021-10-26 19:40:53 -0500

    [update] useEffect, ejercicio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import {Header} from './components/Header'
+import {Character} from './components/Character'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hola mundo</h1>
       <Header/>
+      <Character/>
+      <h1>Hola mundo</h1>
     </div>
   );
 }
