35ee26a316e949022e7d9426095a53f7a56c7eb2
Nicolas <josue.marin1729@gmail.com>
2021-12-09 20:13:57 -0500

    main / fix dark mode for favorite item

--- src/components/FavoriteItem.jsx
+++ src/components/FavoriteItem.jsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, {useContext}from "react";
 import "./FavoriteItem.css"
+import { ThemeContext } from '../Context/ThemeContext';
 
 function FavoriteItem (props) {
   const { fav } = props;
+  const {theme} = useContext(ThemeContext);
   console.log(fav);
   return (
-    <div className='favoriteItem'>
-      <div className= 'favoriteContainer'>
-      <p>{fav.name}</p>
+    <div className={'favoriteItem'}>
+      <div className={'favoriteContainer'}>
+      <p className={"text-"+theme}>{fav.name}</p>
         <figure>
             <img src={fav.image} alt="character" width="80"/>
-            <p>{fav.species}</p>
+            <p className={"text-"+theme}>{fav.species}</p>
         </figure>
       </div>
     </div>
