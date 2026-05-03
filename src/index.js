ab849c287725ccec8fc1555ae7e21802768846fc
Eduardo Jami <edujami524@gmail.com>
2026-05-03 07:33:44 -0500

    refactor: update ReactDOM rendering method to use createRoot for concurrent features

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {ThemeContextProvider} from './Context/ThemeContext';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <ThemeContextProvider>
       <App />
     </ThemeContextProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
